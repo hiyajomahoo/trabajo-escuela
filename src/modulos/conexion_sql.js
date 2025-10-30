01e763080c2795d19c7ff4dd763f7a4c47697e45
pato <muras.patricio@tecnica7.edu.ar>
2025-10-30 17:59:53 -0300

    dockerizado

--- src/modulos/conexion_sql.js
+++ src/modulos/conexion_sql.js
@@ -1,10 +1,12 @@
+import { configDotenv } from 'dotenv';
 import mysql from 'mysql2/promise';
+configDotenv()
 
 const pool = mysql.createPool({
-  host: 'localhost',       
+  host: process.env.DB_HOST,       
   user: 'root',            
-  password: '',            
-  database: 'escuela',     
+  password: process.env.DB_PASSWORD,            
+  database: process.env.DB_NAME,     
   connectionLimit: 5       
 });
 
